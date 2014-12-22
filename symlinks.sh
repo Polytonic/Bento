4de6dcff6e9f53320a0be7ae00b2c3b95fee99b2
Kevin Fung <kfung@tinycranes.com>
2014-12-22 15:22:17 -0500

    Add .gitconfig

--- symlinks.sh
+++ symlinks.sh
@@ -1,3 +1,4 @@
 # Link Configuration Files
 ln -fs "$PWD/sublime.json" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 ln -fs "$PWD/config.fish" ~/.config/fish/
+ln -fs "$PWD/.gitconfig" ~/.gitconfig
