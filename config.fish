a236c9ab566e03f88716d1909b387d1093c49d93
Kevin Fung <kfung@tinycranes.com>
2014-12-23 09:52:15 -0500

    Update Git Prompt

--- config.fish
+++ config.fish
@@ -4,29 +4,15 @@ set -gx PATH $HOMEBREW $PATH
 set -gx fish_greeting (set_color magenta)(date +"%A, %B %d, %Y at %r (%Z)")
 
 # Git Prompt Configuration
-set __fish_git_prompt_showdirtystate "yes"
-set __fish_git_prompt_showstashstate "yes"
-set __fish_git_prompt_showuntrackedfiles "yes"
-set __fish_git_prompt_showupstream "yes"
-set  __fish_git_prompt_showcolorhints "yes"
-
-# need to set bold
-set __fish_git_prompt_color cyan
-set __fish_git_prompt_color_branch blue
-set __fish_git_prompt_color_prefix cyan
-set __fish_git_prompt_color_suffix cyan
-set __fish_git_prompt_color_stagedstate green
-set __fish_git_prompt_color_upstream_ahead green
-set __fish_git_prompt_color_upstream_behind red
-
-# Git Status Characters
-set __fish_git_prompt_char_dirtystate "*"
-set __fish_git_prompt_char_stagedstate "+"
-set __fish_git_prompt_char_untrackedfiles "?"
-set __fish_git_prompt_char_stashstate "\$"
-set __fish_git_prompt_char_upstream_ahead "+"
-set __fish_git_prompt_char_upstream_behind "-"
-set __fish_git_prompt_char_stateseparator " ["
+set __fish_git_prompt_showupstream "verbose"
+set __fish_git_prompt_color_branch blue -o
+set __fish_git_prompt_color_upstream cyan -o
+set __fish_git_prompt_color_suffix cyan -o
+set __fish_git_prompt_char_upstream_ahead "↑"
+set __fish_git_prompt_char_upstream_behind "↓"
+set __fish_git_prompt_char_upstream_diverged "? "
+set __fish_git_prompt_char_upstream_equal "="
+set __fish_git_prompt_char_upstream_prefix " ["
 
 # Override Terminal Title and Set Prompt
 function fish_title; echo $PWD | sed -e "s|^$HOME|~|" | tr -d "\n"; end
