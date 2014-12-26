cc780563eca875ca274faaac3b701a1b857b4cb0
Kevin Fung <kfung@tinycranes.com>
2014-12-25 21:36:04 -0500

    Perform an Unattended Installation

--- homebrew.sh
+++ homebrew.sh
@@ -4,7 +4,7 @@
 export PATH="/usr/local/bin:/usr/local/sbin:$PATH"
 if test ! "$(which brew)"; then
     echo "Installing Homebrew"
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null
 fi
 
 # Update Homebrew
