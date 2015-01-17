63047ceb23e4503fbdc9c8cdd42d0652538dd340
Kevin Fung <kfung@tinycranes.com>
2015-01-17 17:43:44 -0500

    Update for Admin Permissions

--- macosx.sh
+++ macosx.sh
@@ -11,7 +11,13 @@ if [ "$CURRENT_PROFILE" != "$TERM_PROFILE" ]; then
 fi;
 
 # Mute the Startup Chime
-nvram SystemAudioVolume=" "
+sudo nvram SystemAudioVolume=" "
+
+# Disable the Guest User
+sudo defaults write /Library/Preferences/com.apple.loginwindow GuestEnabled -bool false
+
+# Enable the Firewall
+sudo defaults write /Library/Preferences/com.apple.alf globalstate -int 1
 
 # Write Global Settings
 defaults write NSGlobalDomain AppleHighlightColor -string "0.968627 0.831373 1.000000"
@@ -19,8 +25,6 @@ defaults write NSGlobalDomain AppleShowScrollBars -string "WhenScrolling"
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
-defaults write /Library/Preferences/com.apple.loginwindow GuestEnabled -bool false
-defaults write /Library/Preferences/com.apple.alf globalstate -int 1
 defaults write com.apple.systemsound com.apple.sound.beep.sound /System/Library/Sounds/Submarine.aiff
 
 # Write Dock Settings
