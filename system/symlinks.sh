53b8817c7021e7c941f7f406f165eeadba8da11c
Kevin Fung <kfung@tinycranes.com>
2016-03-12 08:53:51 -0800

    Symlink iCloud User Folders

--- system/symlinks.sh
+++ system/symlinks.sh
@@ -1,5 +1,13 @@
 #! /usr/bin/env bash
 
-# Create Symbolic Links
+# Create iCloud Symlink
 ln -s ~/Library/Mobile\ Documents/com\~apple\~CloudDocs ~/iCloud
 chflags -h hidden ~/iCloud
+
+# Create User Folder Symlinks
+mkdir -p ~/iCloud/Documents/ && sudo rm -rf ~/Documents && ln -s ~/iCloud/Documents/ ~/Documents
+mkdir -p ~/iCloud/Downloads/ && sudo rm -rf ~/Downloads && ln -s ~/iCloud/Downloads/ ~/Downloads
+mkdir -p ~/iCloud/Public/    && sudo rm -rf ~/Public    && ln -s ~/iCloud/Public/ ~/Public
+
+# Cleanup Extra Symlink
+rm ~/iCloud/com~apple~CloudDocs
