4374e07fbb7200a988605df66b3b297df46bdc25
Kevin Fung <kfung@tinycranes.com>
2016-04-09 12:12:57 -0700

    Migrate to Dropbox

--- system/symlinks.sh
+++ system/symlinks.sh
@@ -5,16 +5,14 @@ ln -s ~/Library/Mobile\ Documents/com\~apple\~CloudDocs ~/iCloud
 chflags -h hidden ~/iCloud
 
 # Create User Folder Symlinks
-mkdir -p ~/iCloud/Documents/ && sudo rm -rf ~/Documents && ln -s ~/iCloud/Documents/ ~/Documents
-mkdir -p ~/iCloud/Downloads/ && sudo rm -rf ~/Downloads && ln -s ~/iCloud/Downloads/ ~/Downloads
-mkdir -p ~/iCloud/Public/    && sudo rm -rf ~/Public    && ln -s ~/iCloud/Public/ ~/Public
-
-# Symlink the Dropbox Music Folder
-if [ -d ~/Dropbox/iTunes/ ]; then
-    sudo rm -rf ~/Music/iTunes
-    ln -s ~/Dropbox/iTunes/ ~/Music/iTunes
-    chflags hidden ~/Music
+if [ -d ~/Dropbox/ ]; then
+    mkdir -p ~/Dropbox/Desktop/   && sudo rm -rf ~/Desktop      && ln -s ~/Dropbox/Desktop/ ~/Desktop
+    mkdir -p ~/Dropbox/Documents/ && sudo rm -rf ~/Documents    && ln -s ~/Dropbox/Documents/ ~/Documents
+    mkdir -p ~/Dropbox/Downloads/ && sudo rm -rf ~/Downloads    && ln -s ~/Dropbox/Downloads/ ~/Downloads
+    mkdir -p ~/Dropbox/Music/     && sudo rm -rf ~/Music/iTunes && ln -s ~/Dropbox/Music/ ~/Music/iTunes
+    mkdir -p ~/Dropbox/Public/    && sudo rm -rf ~/Public       && ln -s ~/Dropbox/Public/ ~/Public
 fi
 
-# Cleanup Extra Symlink
+# Cleanup Steps
 rm ~/iCloud/com~apple~CloudDocs
+chflags hidden ~/Music
