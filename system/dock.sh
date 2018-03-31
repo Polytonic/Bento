6e888b6d1e251f0ed06f8b53d02b7442ca290c63
Kevin Fung <kfung@tinycranes.com>
2018-03-30 23:55:51 -0700

    Only Show Running Applications in Dock

--- system/dock.sh
+++ system/dock.sh
@@ -6,6 +6,7 @@ defaults write com.apple.dock mouse-over-hilite-stack -bool true
 defaults write com.apple.dock use-new-list-stack -bool true
 defaults write com.apple.dock minimize-to-application -bool true
 defaults write com.apple.dock mru-spaces -bool false
+defaults write com.apple.dock static-only -bool true
 defaults write com.apple.dock showhidden -bool true
 defaults write com.apple.dock mineffect -string "suck"
 defaults write com.apple.dock wvous-bl-corner -int 5
