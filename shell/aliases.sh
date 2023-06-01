1a2ed2d9f3e2ab82ebb1f0d3037eb99ca58b02e5
Kevin Fung <kfung@tinycranes.com>
2023-06-01 16:18:49 -0400

    Add Firewall and Wi-Fi Aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -8,6 +8,12 @@ alias rm="trash"
 # Open the iOS Simulator
 alias simulator="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
 
+# Enable the macOS Firewall
+alias firewall="sudo defaults write /Library/Preferences/com.apple.alf globalstate -bool true"
+
+# Cycle the Wi-Fi Interface
+alias wifi="sudo ifconfig en0 down && sudo ifconfig en0 up"
+
 # Recursively Delete `.DS_Store` Files
 alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
 
